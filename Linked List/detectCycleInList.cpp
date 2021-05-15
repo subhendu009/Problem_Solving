684ca3ee1c846efc6cba831696b65cb369527076
Subhendu Bachaspati <subhendu009@gmail.com>
2021-05-15 18:12:16 +0530

    Detect cycle

---
+++ Linked List/detectCycleInList.cpp
@@ -0,0 +1,38 @@
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Leetcode Problem : 141. Linked List Cycle
+                   https://leetcode.com/problems/linked-list-cycle/                   
+*/
+
+// Definition for singly-linked list.
+struct ListNode 
+{
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+bool hasCycle(ListNode* head)
+{
+    ListNode* slowPtr = head;
+    ListNode* fastPtr = head;
+
+    while (fastPtr && fastPtr->next)
+    {
+        slowPtr = slowPtr->next;
+        fastPtr = fastPtr->next->next;
+
+        if (slowPtr == fastPtr)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
